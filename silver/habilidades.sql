2d8bd3c5a1054a9901f8a0556299b4960ac1aca5
TeoCalvo <TeoCalvo>
2024-09-16 18:54:44 +0000

    Add Habilidades

---
+++ silver/habilidades.sql
@@ -0,0 +1,6 @@
+SELECT id AS idSkill,
+       name AS descNomeSkill,
+       normalized AS descNomeNormalizadoSkill
+
+FROM bronze.trampar_de_casa.skills
+ORDER BY id
--- silver/habilidades_vagas.sql
+++ silver/habilidades_vagas.sql
@@ -1,4 +1,6 @@
 SELECT id AS idVaga,
-       explode(arraySkillsId) AS idHabilidade
+       int(skills) AS idHabilidade
 
 FROM bronze.trampar_de_casa.roles
+
+LATERAL VIEW explode(arraySkillsId) AS skills
