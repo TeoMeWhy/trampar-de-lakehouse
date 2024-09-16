10e97bbc02b9d5d9124443b09d553bf67a547321
TeoCalvo <TeoCalvo>
2024-09-16 19:14:45 +0000

    Fix fill country

--- silver/vagas.sql
+++ silver/vagas.sql
@@ -1,15 +1,20 @@
 SELECT id AS idVaga,
-       topicId AS idTopico,
+       topicId AS idTopico, -- 
        url AS urlVaga,
        description AS descVaga,
        title AS descTituloVaga,
        company AS descEmpresa,
        language AS descLinguagemVaga,
-       country AS descPaisVaga,
+
+       CASE WHEN country IS NULL AND language = 'Portuguese' THEN 'Brasil'
+            WHEN country IS NULL AND language != 'Portuguese' THEN 'Internacional'
+            ELSE country
+       END AS descPaisVaga,
+       
        currency AS descMoedaVaga,
        salary AS vlSalario,
-       ready AS descPronto,
-       minimumYears AS vlExpMinima,
+       CASE WHEN ready = 1 THEN 1 ELSE 0 END AS descPronto, -- vaga disponível no site
+       minimumYears AS vlExpMinima, -- Se exige exp mínima
        updatedAt AS dtAtualizacao,
        createdAt AS dtCriacao
 
